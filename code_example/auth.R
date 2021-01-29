e3e84692a13cd2d837632043c12d5759bf1f4c79
Alexey Seleznev <selesnow@gmail.com>
2021-01-29 09:37:26 +0200

    Add files via upload

---
+++ code_example/auth.R
@@ -0,0 +1,37 @@
+# vk auth
+# install.packages('rvkstat')
+library(rvkstat)
+
+# простая авторизация
+vkAuth(username   = 'selesnow', 
+       token_path = 'c:/vkauth_data')
+
+# запрашиваем список аккаунтов
+accounts <- vkGetAdAccounts(username   = 'selesnow', 
+                            token_path = 'c:/vkauth_data')
+
+# задаём опции
+vkSetUsername('selesnow')
+vkSetTokenPath('c:/vkauth_data')
+
+# запрашиваем список аккаунтов
+accounts_2 <- vkGetAdAccounts()
+
+
+# авторизация через собственное приложение --------------------------------
+vkAuth(app_id     = ,
+       app_secret = )
+
+# переменные среды и файл .Renviron ---------------------------------------
+# переменные среды
+Sys.setenv(
+  RVK_USER = "selesnow", 
+  RVK_TOKEN_PATH = "c:/vkauth_data"
+)
+
+# подключение пакета
+library(rvkstat)
+
+# запрашиваем список аккаунтов
+accounts_3 <- vkGetAdAccounts()
+
